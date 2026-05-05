c389de1faf9a05c7faf57e875136454828320f59
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-05-05 10:12:15 +0200

    Fix Uno sample desktop launch

--- samples/ProText.Uno.Sample/App.cs
+++ samples/ProText.Uno.Sample/App.cs
@@ -1,15 +1,24 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace ProText.Uno.Sample;
 
-public sealed class App : Application
+public sealed partial class App : Application
 {
     private Window? _window;
 
+    public App()
+    {
+        InitializeComponent();
+    }
+
     protected override void OnLaunched(LaunchActivatedEventArgs args)
     {
+        Resources.MergedDictionaries.Add(new XamlControlsResources());
+
         _window = new Window
         {
+            Title = "ProText.Uno.Sample",
             Content = new MainPage()
         };
 
